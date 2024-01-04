880dcaf0e19f9945465bcd79c4e222d70fb112a9
Cristian Cassetta <cristiancassetta@gmail.com>
2024-01-04 09:37:15 +0100

    Added bootstrap, a navbar and a few templates

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return render_template("index.html")
+
+@app.route("/add-experiment")
+def addExperiment():
+  return render_template("add_experiment.html")
+ 
+
